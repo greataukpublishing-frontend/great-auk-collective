39c62da372648aebee03c33cc4ccaaad973c5524
greataukpublishing-frontend <greataukpublishing@gmail.com>
2026-03-09 15:19:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-```tsx
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -121,4 +120,4 @@ const App = () => (
 );
 
 export default App;
-```
+
