a2b759cc2b6884ae9d3b9268593adff301b22776
greataukpublishing-frontend <greataukpublishing@gmail.com>
2026-03-09 15:35:11 +0100

    Create cartpage.tsx

---
+++ src/pages/cartpage.tsx
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">My Cart</h1>
+      <p>Your cart is empty.</p>
+    </div>
+  );
+}
